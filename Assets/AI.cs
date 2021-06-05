2faace9e540f2df6b47f640c3eef2e27fc9ba44f
ogumusay <gumusayogulcan@gmail.com>
2021-06-05 15:12:21 +0300

    v0.2a
    simple state machine

--- Assets/AI.cs
+++ Assets/AI.cs
@@ -2,29 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class AI : MonoBehaviour
 {
     public Transform[] waypoints;
+
+    public NavMeshAgent agent;
 
     State currentState;
     public Transform player;
     float detectionDegree = 60f;
     public float detectionDistance = 7f;
     public float escapeDistance = 9f;
-    Color lineColor = Color.green;
+    public Color lineColor = Color.green;
 
     [SerializeField] Text playerText;
     [SerializeField] Text botText;
 
     void Start()
     {
-        currentState = new Idle(this, player);
+        agent = GetComponent<NavMeshAgent>();
+        currentState = new Idle(this, player, agent);
     }
 
     void Update()
     {
         currentState = currentState.Process();       
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.name);                
+            }
+        }
 
         DebugDraw();
     }
@@ -44,11 +60,7 @@ public class AI : MonoBehaviour
 
         Debug.DrawLine(playerPos, transform.position, lineColor);
 
-        if (angle <= detectionDegree && distance < detectionDistance)
-        {
-            lineColor = Color.red;
-        }   
-        else if(distance > escapeDistance)
+        if(distance > escapeDistance)
         {
             lineColor = Color.green;
         }
--- Assets/State.cs
+++ Assets/State.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 
 public abstract class State
@@ -17,12 +18,14 @@ public abstract class State
     public EVENT currentEvent;
     public State nextState;
     public AI bot;
+    public NavMeshAgent agent;
     public Transform player;
    
-    public State(AI bot, Transform player)
+    public State(AI bot, Transform player, NavMeshAgent agent)
     {
         this.bot = bot;
         this.player = player;
+        this.agent = agent;
     }
 
     public State Process()
@@ -60,12 +63,15 @@ public abstract class State
 public class Idle : State
 {
     public int idleTime = 0;
+    public int MinIdleTime = 2;
+    public int MaxIdleTime = 4;
     public float timer = 0f;
+    public float rotationSpeed = 20f;
 
     public float detectionDegree = 60f;
     public float detectionDistance = 7f;
 
-    public Idle(AI bot, Transform player) : base (bot, player)
+    public Idle(AI bot, Transform player, NavMeshAgent agent) : base (bot, player, agent)
     {
         state = STATE.IDLE;
     }
@@ -73,7 +79,7 @@ public class Idle : State
     public override void Enter()
     {
         //Random waiting time for Idle event
-        idleTime = Random.Range(1, 4);
+        idleTime = Random.Range(MinIdleTime, MaxIdleTime + 1);
         base.Enter();
     }
 
@@ -81,16 +87,30 @@ public class Idle : State
     {
         timer += Time.deltaTime;
 
+        LookAround();
+
         //When finished idle time
-        if(timer >= idleTime)
+        if (timer >= idleTime)
         {
-            nextState = new Patrol(bot, player);
+            nextState = new Patrol(bot, player, agent);
             currentEvent = EVENT.EXIT;
         }
-        
+
         DistanceDetection();
 
         base.Update();
+    }
+
+    private void LookAround()
+    {
+        if (timer < idleTime / 2)
+        {
+            bot.transform.Rotate(new Vector3(0f, rotationSpeed * Time.deltaTime, 0f), Space.World);
+        }
+        else
+        {
+            bot.transform.Rotate(new Vector3(0f, -rotationSpeed * Time.deltaTime, 0f), Space.World);
+        }
     }
 
     //Measure distance between player and bot
@@ -101,11 +121,28 @@ public class Idle : State
         float angle = Vector3.Angle(playerPos - bot.transform.position, bot.transform.forward);
         float distance = Vector3.Distance(playerPos, bot.transform.position);
 
-        if (angle <= detectionDegree && distance < detectionDistance)
+        if (distance < detectionDistance)
         {
-            //DETECTED
-            nextState = new Chase(bot, player);
-            currentEvent = EVENT.EXIT;
+            if (angle <= detectionDegree)
+            {
+                Vector3 dir = (playerPos - bot.transform.position);
+                
+                Ray ray = new Ray(bot.transform.position, dir);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    bot.lineColor = Color.yellow;
+
+                    if (hit.transform.CompareTag("Player"))
+                    {
+                        //DETECTED
+                        nextState = new Chase(bot, player, agent);
+                        bot.lineColor = Color.red;
+                        currentEvent = EVENT.EXIT;                        
+                    }
+                }
+            }
         }        
     }
     public override void Exit()
@@ -117,13 +154,13 @@ public class Idle : State
 public class Patrol : State
 {
     public int waypointIndex = 0;
-    public float speed = 5f;
+    public float speed = 3f;
     public float rotationSpeed = 4f;
 
     public float detectionDegree = 60f;
     public float detectionDistance = 7f;
 
-    public Patrol(AI bot, Transform player) : base (bot, player)
+    public Patrol(AI bot, Transform player, NavMeshAgent agent) : base (bot, player, agent)
     {
         state = STATE.PATROL;
     }
@@ -132,13 +169,14 @@ public class Patrol : State
     public override void Enter()
     {
         waypointIndex = Random.Range(0, 4);
+        agent.speed = speed;
         base.Enter();
     }
 
     public void PatrolWaypoint()
     {
         //Patrol
-        if (Vector3.Distance(bot.transform.position, bot.waypoints[waypointIndex].position) < 2f)
+        if (Vector3.Distance(bot.transform.position, bot.waypoints[waypointIndex].position) < 1f)
         {
             int previousIndex = waypointIndex;
             //Set random waypoint
@@ -158,17 +196,21 @@ public class Patrol : State
             // 40% chance to be idle on waypoint
             if(Random.Range(0, 100) < 40)
             {
-                nextState = new Idle(bot, player);
+                nextState = new Idle(bot, player, agent);
                 currentEvent = EVENT.EXIT;
                 return;
             }
         }
 
+        agent.SetDestination(bot.waypoints[waypointIndex].position);    
+
+        /*
         //Movement
         bot.transform.Translate(bot.transform.forward * speed * Time.deltaTime, Space.World);
 
         Quaternion lookAtWP = Quaternion.LookRotation(bot.waypoints[waypointIndex].position - bot.transform.position);
-        bot.transform.rotation = Quaternion.Slerp(bot.transform.rotation, lookAtWP, Time.deltaTime * rotationSpeed);        
+        bot.transform.rotation = Quaternion.Slerp(bot.transform.rotation, lookAtWP, Time.deltaTime * rotationSpeed);       
+        */ 
     }
 
     //Measure distance between player and bot
@@ -179,24 +221,51 @@ public class Patrol : State
         float angle = Vector3.Angle(playerPos - bot.transform.position, bot.transform.forward);
         float distance = Vector3.Distance(playerPos, bot.transform.position);
 
+        if (distance < detectionDistance)
+        {
+            if (angle <= detectionDegree)
+            {
+                Vector3 dir = (playerPos - bot.transform.position);
+                
+                Ray ray = new Ray(bot.transform.position, dir);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    bot.lineColor = Color.yellow;
+
+                    if (hit.transform.CompareTag("Player"))
+                    {
+                        //DETECTED
+                        nextState = new Chase(bot, player, agent);
+                        bot.lineColor = Color.red;
+                        currentEvent = EVENT.EXIT;                        
+                    }
+                }
+            }
+        }  
+
+
+/*
         if (angle <= detectionDegree && distance < detectionDistance)
         {
             //DETECTED
-            nextState = new Chase(bot, player);
+            nextState = new Chase(bot, player, agent);
             currentEvent = EVENT.EXIT;
-        }        
+        }    
+        */    
     }
 
     public override void Update()
     {
         if(Input.GetKeyDown(KeyCode.I))
         {
-            nextState = new Idle(bot, player);
+            nextState = new Idle(bot, player, agent);
             currentEvent = EVENT.EXIT;
         }
         else if(Input.GetKeyDown(KeyCode.A))
         {
-            nextState = new Attack(bot, player);
+            nextState = new Attack(bot, player, agent);
             currentEvent = EVENT.EXIT;
         }
 
@@ -216,8 +285,9 @@ public class Attack : State
 {
     public float attackDistance = 3f;
     public float attackDegree = 20f;
+    public float rotationSpeed = 5f;
 
-    public Attack(AI bot, Transform player) : base (bot, player)
+    public Attack(AI bot, Transform player, NavMeshAgent agent) : base (bot, player, agent)
     {
         state = STATE.ATTACK;
     }
@@ -231,7 +301,7 @@ public class Attack : State
     {
         if(Input.GetKeyDown(KeyCode.I))
         {
-            nextState = new Idle(bot, player);
+            nextState = new Idle(bot, player, agent);
             currentEvent = EVENT.EXIT;
         }
         
@@ -252,12 +322,18 @@ public class Attack : State
         float distance = Vector3.Distance(playerPos, bot.transform.position);
         float angle = Vector3.Angle(playerPos - bot.transform.position, bot.transform.forward);
 
-        if (distance >= attackDistance || angle > attackDegree)
+        if (distance >= attackDistance)
         {
             //DETECTED
-            nextState = new Chase(bot, player);
+            nextState = new Chase(bot, player, agent);
             currentEvent = EVENT.EXIT;
         }  
+
+        if (angle > attackDegree)
+        {
+            Quaternion lookAtWP = Quaternion.LookRotation(playerPos - bot.transform.position);
+            bot.transform.rotation = Quaternion.Slerp(bot.transform.rotation, lookAtWP, Time.deltaTime * rotationSpeed);   
+        }
     }
 }
 
@@ -270,13 +346,14 @@ public class Chase : State
     public float attackDistance = 3f;
     public float escapeDistance = 9f;
 
-    public Chase(AI bot, Transform player) : base (bot, player)
+    public Chase(AI bot, Transform player, NavMeshAgent agent) : base (bot, player,agent)
     {
         state = STATE.CHASE;
     }
 
     public override void Enter()
     {
+        agent.speed = speed;
         base.Enter();
     }
 
@@ -284,7 +361,7 @@ public class Chase : State
     {
         if(Input.GetKeyDown(KeyCode.I))
         {
-            nextState = new Idle(bot, player);
+            nextState = new Idle(bot, player,agent);
             currentEvent = EVENT.EXIT;
         }
         
@@ -296,10 +373,14 @@ public class Chase : State
 
     void Pursue()
     {
+        agent.SetDestination(player.position);
+
+        /*
         bot.transform.Translate(bot.transform.forward * speed * Time.deltaTime, Space.World);
 
         Quaternion lookAtWP = Quaternion.LookRotation(player.position - bot.transform.position);
         bot.transform.rotation = Quaternion.Slerp(bot.transform.rotation, lookAtWP, Time.deltaTime * rotationSpeed);  
+        */
     }
 
     void DistanceDetection()
@@ -309,16 +390,17 @@ public class Chase : State
         float angle = Vector3.Angle(playerPos - bot.transform.position, bot.transform.forward);
         float distance = Vector3.Distance(playerPos, bot.transform.position);
 
-        if (angle <= attackDegree && distance < attackDistance)
+        if (distance < attackDistance)
         {
             //DETECTED
-            nextState = new Attack(bot, player);
+            agent.ResetPath();
+            nextState = new Attack(bot, player, agent);
             currentEvent = EVENT.EXIT;
         }  
 
         if (distance >= escapeDistance)
         {
-            nextState = new Patrol(bot, player);
+            nextState = new Patrol(bot, player, agent);
             currentEvent = EVENT.EXIT;
         }
     }
